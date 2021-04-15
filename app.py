3aaedb0aa75b3839d721f50d8211cdce025ee691
Barry <bbuehne@gmail.com>
2021-04-15 00:36:41 -0500

    lots of changes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from werkzeug.exceptions import abort
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from firebase_admin import db
 
 from tlcolor import RGB
 
@@ -10,7 +11,7 @@ from tlcolor import RGB
 # Firebase
 def connect_db():
     cred = credentials.ApplicationDefault()
-    firebase_admin.initialize_app(cred, {
+    default_app = firebase_admin.initialize_app(cred, {
         'project_id': 'tlcolormatch',
     })
 
@@ -21,8 +22,10 @@ def connect_db():
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=('GET','POST'))
 def index():
+    app.name = 'tlcolormatch'
+    app.config['SECRET_KEY'] = 'lkasd83ksu39si23'
     if request.method == 'POST':
         name = request.form['title']
         sensor_data = request.form['sensordata']
@@ -30,14 +33,31 @@ def index():
         if not name:
             flash('Name is required')
         else:
-            db = connect_db()
-            lines = sensor_data.splitline()
+
+            lines = sensor_data.splitlines()
             for line in lines:
                 if 'sRGB' not in line:
                     continue
                 else:
                     rgb = line.split(':')[1][1:].split(',')
-                    doc_ref = db.collection('samples').document(name)
+
+    return render_template('index.html')
+
+@app.route('/color_samples', methods=('GET','POST'))
+def color_samples():
+    if request.method == 'POST':
+        name = request.form['title']
+        sensor_data = request.form['sensordata']
+        if not name:
+            flash('Name is required')
+        else:
+            lines = sensor_data.splitlines()
+            for line in lines:
+                if 'sRGB' not in line:
+                    continue
+                else:
+                    rgb = line.split(':')[1][1:].split(',')
+                    doc_ref = fdb.collection('samples').document(name)
                     doc_ref.set({
                         'name' : name,
                         'r' : int(rgb[0]),
@@ -45,11 +65,20 @@ def index():
                         'b' : int(rgb[2])
                     })
                     return redirect(url_for('color_samples'))
-    return render_template('index.html')
 
-@app.route('/color_samples')
-def color_samples():
-    return render_template('color_samples.html')
+    # retrieve samples
+    doc_ref = fdb.collection('samples')
+    samples = doc_ref.get()
+#    print(dir(samples))
+    sample_list = {}
+    for sample in samples:
+        sample_list[sample.to_dict()['name']] = sample.to_dict()
+    print(sorted(sample_list))
+    samples = [ sample_list[x] for x in sorted(sample_list)]
+    print(samples)
 
+    return render_template('color_samples.html', samples=samples)
+
+fdb = connect_db()
 if __name__ == '__main__':
     app.run()
